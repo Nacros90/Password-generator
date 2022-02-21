08573a8ecb7a1f68fffbb366a08a39b489c2e9c3
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-02-21 23:42:32 +0100

    Create Password generator.py
    écriture de la base du programme, mais il va falloir apprendre à utiliser le dictionnaire

---
+++ Password generator.py
@@ -0,0 +1,11 @@
+import random
+from re import A
+        #afficher les instructions ici
+
+        #Il faut utiliser un dictionnaire
+
+start=1
+while start==1:
+    nb_cara=int(input("Nombre de caractére du MDP ?"))
+    for x in range(0,nb_cara):
+        n=random.randit(1,26)
