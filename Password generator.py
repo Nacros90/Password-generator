b1cd66a5a0577f95465df4e20a3259cf57145e63
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-03-02 22:44:03 +0100

    Système de génération du MDP
    J'ai utilisé une liste, car je n'arrive pas à associé un chiffre à une lettre.

--- Password generator.py
+++ Password generator.py
@@ -3,9 +3,14 @@ from re import A
         #afficher les instructions ici
 
         #Il faut utiliser un dictionnaire ou des listes, le type de variable str ne fonctionne pas
-
+MDP=[]
 start=1
 while start==1:
     nb_cara=int(input("Nombre de caractére du MDP ?"))
     for x in range(0,nb_cara):
-        n=random.randit(1,26)
+        n=random.randint(1,2)
+        if n==1:
+            MDP.append("a")
+        elif n==2:
+            MDP.append("b")
+    print(MDP)
